4cf251c98b128388bba174eaa6e073813622f53e
Brandon <bshaven7@gmail.com>
2024-09-25 11:48:06 -0400

    Added first item "Pink Garnet" Set up to handle items in the future

--- src/main/java/com/bshaven/lucidtech/LucidTech.java
+++ src/main/java/com/bshaven/lucidtech/LucidTech.java
@@ -1,5 +1,6 @@
 package com.bshaven.lucidtech;
 
+import com.bshaven.lucidtech.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,6 @@ public class LucidTech implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems(); // Registers ModItems Class
 	}
 }
--- src/main/java/com/bshaven/lucidtech/item/ModItems.java
+++ src/main/java/com/bshaven/lucidtech/item/ModItems.java
@@ -1,7 +1,27 @@
 package com.bshaven.lucidtech.item;
 
+import com.bshaven.lucidtech.LucidTech;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
 public class ModItems {
 
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
 
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(LucidTech.MOD_ID, name), item); // Register Item Helper Method
+    }
+
+    public static void registerModItems() {
+        LucidTech.LOGGER.info("Registering Mod Items for " + LucidTech.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+        });
+    }
 
 }
