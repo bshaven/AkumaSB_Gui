e5140ee31971e7ec34ccc81f0b5c97c1d5b85485
Brandon <bshaven7@gmail.com>
2024-09-25 10:19:14 -0400

    Fixed master class

--- src/main/java/com/bshaven/akumasbgui/AkumaSBGui.java
+++ src/main/java/com/bshaven/akumasbgui/AkumaSBGui.java
@@ -1,16 +1,18 @@
 package com.bshaven.akumasbgui;
 
 import net.fabricmc.api.ModInitializer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.minecraft.server.MinecraftServer;
 
 public class AkumaSBGui implements ModInitializer {
-	public static final String MOD_ID = "akumasb-gui";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+	public static final String MOD_ID = "akumasbgui";
 	@Override
 	public void onInitialize() {
+		// This code runs when your mod is initialized
+		ServerLifecycleEvents.SERVER_STARTED.register(this::onServerStarted);
+	}
 
+	private void onServerStarted(MinecraftServer server) {
+		System.out.println("Hello Fabric world!");
 	}
 }
